9ee2aacbe4d62175ad558b0586ddc148fe590129
Eric Njuki <23366230+ericnjuki@users.noreply.github.com>
2022-03-06 16:34:45 +0300

    Updates

---
+++ precache-manifest.59ac1dac10511265d8ffe45257012abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c5193b411df9e7b4d53cabdd1ba1ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cf37484400afb100420",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "47ea22dea820fc672399",
+    "url": "./static/css/main.c2209f96.chunk.css"
+  },
+  {
+    "revision": "8cf37484400afb100420",
+    "url": "./static/js/2.109fb3ea.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.109fb3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ea22dea820fc672399",
+    "url": "./static/js/main.37b4c443.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
